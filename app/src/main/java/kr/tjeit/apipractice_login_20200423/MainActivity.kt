cf15fa9cbd763ba3c7b3173a805a9148d71b8c69
RobertHan96 <yshan4329@gmail.com>
2020-04-27 21:02:53 +0900

    - 메인 액티비티, 로그인 액티비티 분리 - 로그인한 유저 이름을 출력하기 위한 텍스트뷰 생성

--- app/src/main/java/kr/tjeit/apipractice_login_20200423/MainActivity.kt
+++ app/src/main/java/kr/tjeit/apipractice_login_20200423/MainActivity.kt
@@ -2,14 +2,8 @@ package kr.tjeit.apipractice_login_20200423
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
-import kr.tjeit.apipractice_login_20200423.utils.ServerUtil
-import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,39 +13,9 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
-        loginBtn.setOnClickListener {
-            val inputId = idEdt.text.toString() // cho881020 => 조경진
-            val inputPw = pwEdt.text.toString() // asdf1234
-
-//            서버로 로그인 요청 => ServerUtil클래스 기능 활용
-            ServerUtil.postRequestLogin(mContext, inputId, inputPw, object : ServerUtil.JsonResponseHandler {
-                override fun onResponse(json: JSONObject) {
-//                    실제로 응답을 받은걸 분석해서 => 대응
-
-//                    임시로 서버 응답 확인 하기 위한 코드
-//                    Log.d("서버응답JSON", json.toString())
-                    val code = json.getInt("code")
-                    if (code == 200){
-//                        로그인 성공시 처리
-
-                    } else {
-                        val message = json.getString("message")
-//                        UI를 그리는 작업은 메인스레드에서 실행이 안되므로, runOnUiThread 메소드 안에서 호출
-                        runOnUiThread {
-                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
-                        }
-
-                    }
-                }
-
-            })
-
-        }
-
     }
 
     override fun setValues() {
 
     }
-
 }
