165c3f18f1b438511685e778f7ae11bbee4d103f
RobertHan96 <yshan4329@gmail.com>
2020-04-27 21:12:53 +0900

    - 로그인 성공시 사용자 이름 추출 후 표기

--- app/src/main/java/kr/tjeit/apipractice_login_20200423/MainActivity.kt
+++ app/src/main/java/kr/tjeit/apipractice_login_20200423/MainActivity.kt
@@ -2,8 +2,11 @@ package kr.tjeit.apipractice_login_20200423
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+    private lateinit var userName : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +19,8 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        userName = intent.getStringExtra("userName")
+        nameText.text = userName
 
     }
 }
